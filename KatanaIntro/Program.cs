7b0c5c2cd02f0396ad602ccf2ae9a9f0bdb94c27
Timmy Tom <timmy.tom@cba.com.au>
2017-06-21 16:42:37 +1000

    Add project files.

---
+++ KatanaIntro/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Owin.Hosting;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KatanaIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string uri = "http://localhost:8080";
+            using (WebApp.Start<Startup>(uri))
+            {
+                Console.WriteLine("My First Katana App Started");
+                Console.ReadKey();
+                Console.WriteLine("My First Katana App Stopping");
+            }
+        }
+    }
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseWelcomePage();
+            //app.Run(ctx =>
+            //{
+            //    return ctx.Response.WriteAsync("Hello World");
+            //});
+        }
+    }
+}
